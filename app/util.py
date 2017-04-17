e413c444bd43238ef331fad4fae88bd95cfd2fc2
Swalloow <joon920570@gmail.com>
2017-04-17 11:01:01 +0900

    Parrot with RTM api

---
+++ app/util.py
@@ -0,0 +1,14 @@
+# Constant variables
+BOT_NAME = "parrot-bot"
+
+
+def get_bot_id(sc):
+    api_call = sc.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+    else:
+        print("could not find bot user with the name " + BOT_NAME)
