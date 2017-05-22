b220e356f7bf32307b64c294f9c30d5581d571f6
cookieshake <cookieshake@hotmail.com>
2017-05-22 11:10:48 +0900

    fix handle_error method

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import os
 BOT_NAME = "parrot-bot"
 BOT_ID = os.environ["BOT_ID"]
 AT_BOT = "<@" + BOT_ID + ">"
-
+EXAMPLE_COMMAND = "환율"
 
 def get_bot_id(sc):
     api_call = sc.api_call("users.list")
@@ -31,9 +31,11 @@ def parse_slack_output(slack_rtm_output):
                        output['channel']
     return None, None
 
-def handle_error(sc):
-    response = "(local)뭐라는거야. 이런 명령어를 쓰도록 해 *" + EXAMPLE_COMMAND + "*"
+
+def handle_error(result, channel, sc):
+    response = "음...뭔가 잘못 되었어 :sadparrot: {}".format(result['message'])
     sc.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+
 
 def parrot_says(result, channel, sc):
     """
@@ -42,7 +44,6 @@ def parrot_says(result, channel, sc):
         returns back what it needs for clarification.
     """
 
-    EXAMPLE_COMMAND = "환율"
     response = "뭐라는거야. 이런 명령어를 쓰도록 해 *" + EXAMPLE_COMMAND + "*"
     if result['status'] == 'OK':
         response = "현재 환율은 {} 이래 :fastparrot:".format(result['data'])
