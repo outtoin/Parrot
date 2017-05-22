23f637499afa0f71bbc9632bcb68487c95490278
cookieshake <cookieshake@hotmail.com>
2017-05-22 11:33:52 +0900

    enhance stability with try-except

--- app.py
+++ app.py
@@ -32,18 +32,22 @@ if __name__ == "__main__":
     if sc.rtm_connect():
         print("Parrot-Bot connected and running!")
         while True:
-            command, channel = parse_slack_output(sc.rtm_read())
-            success = 0 # if all model failed, command is not valid
+            try:
+                command, channel = parse_slack_output(sc.rtm_read())
+                success = 0 # if all model failed, command is not valid
 
-            result = response(command)
-            if result:
-                if result['status'] == 'OK':
-                    parrot_says(result, channel, sc)
+                result = response(command)
+                if result:
+                    if result['status'] == 'OK':
+                        parrot_says(result, channel, sc)
+                    else:
+                        handle_error(result, channel, sc)
                 else:
-                    handle_error(result, channel, sc)
-            else:
-                result = dict(status='error', message='뭐라는거야. 이런 명령어를 쓰도록 해 *' + EXAMPLE_COMMAND + "*")
-                handle_error(result, channel, sc)  # if command is '', handle_error wouldn't be executed
+                    result = dict(status='error', message='뭐라는거야. 이런 명령어를 쓰도록 해 *' + EXAMPLE_COMMAND + "*")
+                    handle_error(result, channel, sc)  # if command is '', handle_error wouldn't be executed
+            except:
+                result = dict(status='error')
+                handle_error(result, channel, sc)
 
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
